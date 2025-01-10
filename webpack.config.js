82f35cea57dec3227b136aad8f3f7be4f2427d92
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-01-10 13:21:16 -0400

    core: Estrutura inicial do projeto. Configurado Webpack + babel, sass e boostrap

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Limpa a pasta dist antes de cada build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Caminho para o HTML base
+      filename: 'index.html',   // Nome do arquivo gerado na pasta dist
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), 
+    },
+    compress: true,
+    port: 3000,
+    watchFiles: {
+      paths: ['src/**/*', 'index.html'], 
+    },
+  },
+};

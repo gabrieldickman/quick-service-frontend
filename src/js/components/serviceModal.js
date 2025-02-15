767f7c968b2d2900bc423ae7da7eb733800531d8
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-02-15 18:18:25 -0400

    feat: implementado funcionalidade de copy to clipboard das informações

--- src/js/components/serviceModal.js
+++ src/js/components/serviceModal.js
@@ -9,73 +9,63 @@ export function gerarAtendimento() {
     const modalContent = document.querySelector(".modal-atendimento-content");
     const serviceContent = `
 
-    [INFORMAÇÕES DO ATENDIMENTO] <br>
-    <br>
-
-    ATENDENTE: ${document.querySelector("#nome-atendente").value} <br>
-    DATA DO ATENDIMENTO: ${document.querySelector("#data-atendimento").value} <br>
-    PROTOCOLO: ${document.querySelector("#protocolo-atendimento").value} <br>
-    CONTATANTE: ${document.querySelector("#nome-contatante").value} <br>
-    CONTATO: ${document.querySelector("#telefone").value} <br>
-    TITULAR: ${document.querySelector("#titular").value} <br>
-    <br>
-
-    [INFORMAÇÕES DO CONCENTRADOR]<br>
-    <br>
-
-    OLT ${document.querySelector("#nome_olt").value} SLOT ${document.querySelector("#slot").value} PON ${document.querySelector("#pon").value} <br>
-    SINAL DA FIBRA: ${document.querySelector("#sinal-fibra").value} <br>
-    PON DO EQUIPAMENTO: ${document.querySelector("#mac_equipamento").value} <br>
-    REDE LAN: ${document.querySelector("#rede-lan-select").value}<br>
-    PLANO: ${document.querySelector("#plano_cliente").value}<br>
-    LOGIN: ${document.querySelector("#login_pppoe").value}<br>
-    SENHA: ${document.querySelector("#senha_pppoe").value}<br>
-    <br>
-
-    [INFORMAÇÕES DO CHAMADO]<br>
-    <br>
-
-    MOTIVO DO CHAMADO: ${document.querySelector("#motivo-do-chamado").value}<br>
-    DESCRIÇÃO DO ATENDIMENTO: ${document.querySelector("#descricao_atendimento").value}<br>
-    <br>
-
-    [MÉTODOS APLICADOS NO EQUIPAMENTO]<br>
-    <br>
-
-    GERAL<br>
-    ATUALIZAÇÃO DE FIRMWARE: ${document.querySelector("#atualizado-firmware-sim").checked ? "SIM" : "NÃO"} <br>
-    LIMPEZA DE MAC: ${document.querySelector("#limpeza-mac-sim").checked ? "SIM" : "NÃO"} <br>
-    ALTERADO SNTP: ${document.querySelector("#alterado-sntp-sim").checked ? "SIM" : "NÃO"} <br>
-    SETADO DNS: ${document.querySelector("#setado-dns-sim").checked ? "SIM" : "NÃO"} <br>
-    UPnP: ${document.querySelector("#habilitado-upnp-sim").checked ? "LIGADO" : "DESLIGADO"} <br>
-    FIREWALL: ${document.querySelector('input[name="firewall"]:checked').value.toUpperCase()} <br>
-    ALG: ${document.querySelector('input[name="alg"]:checked').value.toUpperCase()} <br>
-    <br>
-
-    REDE 2.4G<br>
-    ALTERADO CANAL: ${document.querySelector("#canal-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#canal-2g-select-1").value} PARA ${document.querySelector("#canal-2g-select-2").value}`}  <br>
-    ALTERADO MODO: ${document.querySelector("#modo-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#modo-2g-select-1").value} PARA ${document.querySelector("#modo-2g-select-2").value}`} <br>
-    ALTERADO LARGURA DA BANDA: ${document.querySelector("#largura-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#largura-2g-select-1").value} PARA ${document.querySelector("#largura-2g-select-2").value}`} <br>
-    ALTERADO SGI: ${document.querySelector("#sgi-2g-ligado").checked ? "LIGADO" : "DESLIGADO"} <br>
-    ALTERADO ENCRIPTAÇÃO: ${document.querySelector("#enctriptacao-2g-alterada-sim").checked ? "SIM" : "NÃO"} <br>
-    <br>
-
-    REDE 5G<br>
-    ALTERADO CANAL: ${document.querySelector("#canal-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#canal-5g-select-1").value} PARA ${document.querySelector("#canal-5g-select-2").value}`}  <br>
-    ALTERADO MODO: ${document.querySelector("#modo-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#modo-5g-select-1").value} PARA ${document.querySelector("#modo-5g-select-2").value}`} <br>
-    ALTERADO LARGURA DA BANDA: ${document.querySelector("#largura-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#largura-5g-select-1").value} PARA ${document.querySelector("#largura-5g-select-2").value}`} <br>
-    ALTERADO SGI: ${document.querySelector("#sgi-5g-ligado").checked ? "LIGADO" : "DESLIGADO"} <br>
-    ALTERADO ENCRIPTAÇÃO: ${document.querySelector("#enctriptacao-5g-alterada-sim").checked ? "SIM" : "NÃO"} <br>
-    <br>
-    `
+    [INFORMAÇÕES DO ATENDIMENTO]
     
+    ATENDENTE: ${document.querySelector("#nome-atendente").value} 
+    DATA DO ATENDIMENTO: ${document.querySelector("#data-atendimento").value} 
+    PROTOCOLO: ${document.querySelector("#protocolo-atendimento").value} 
+    CONTATANTE: ${document.querySelector("#nome-contatante").value} 
+    CONTATO: ${document.querySelector("#telefone").value} 
+    TITULAR: ${document.querySelector("#titular").value} 
+    ------------------------------------------------------------------------
+    [INFORMAÇÕES DO CONCENTRADOR]
+
+    OLT ${document.querySelector("#nome_olt").value} SLOT ${document.querySelector("#slot").value} PON ${document.querySelector("#pon").value} 
+    SINAL DA FIBRA: ${document.querySelector("#sinal-fibra").value} 
+    PON DO EQUIPAMENTO: ${document.querySelector("#mac_equipamento").value} 
+    REDE LAN: ${document.querySelector("#rede-lan-select").value}
+    PLANO: ${document.querySelector("#plano_cliente").value}
+    LOGIN: ${document.querySelector("#login_pppoe").value}
+    SENHA: ${document.querySelector("#senha_pppoe").value}
+    ------------------------------------------------------------------------
+    [INFORMAÇÕES DO CHAMADO]
+
+    MOTIVO DO CHAMADO: ${document.querySelector("#motivo-do-chamado").value}
+    DESCRIÇÃO DO ATENDIMENTO: ${document.querySelector("#descricao_atendimento").value}
+    ------------------------------------------------------------------------
+    [MÉTODOS APLICADOS NO EQUIPAMENTO]
+
+    GERAL 
+    ATUALIZAÇÃO DE FIRMWARE: ${document.querySelector("#atualizado-firmware-sim").checked ? "SIM" : "NÃO"} 
+    LIMPEZA DE MAC: ${document.querySelector("#limpeza-mac-sim").checked ? "SIM" : "NÃO"} 
+    ALTERADO SNTP: ${document.querySelector("#alterado-sntp-sim").checked ? "SIM" : "NÃO"} 
+    SETADO DNS: ${document.querySelector("#setado-dns-sim").checked ? "SIM" : "NÃO"} 
+    UPnP: ${document.querySelector("#habilitado-upnp-sim").checked ? "LIGADO" : "DESLIGADO"} 
+    FIREWALL: ${document.querySelector('input[name="firewall"]:checked').value.toUpperCase()} 
+    ALG: ${document.querySelector('input[name="alg"]:checked').value.toUpperCase()} 
+
+    REDE 2.4G 
+    ALTERADO CANAL: ${document.querySelector("#canal-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#canal-2g-select-1").value} PARA ${document.querySelector("#canal-2g-select-2").value}`}  
+    ALTERADO MODO: ${document.querySelector("#modo-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#modo-2g-select-1").value} PARA ${document.querySelector("#modo-2g-select-2").value}`} 
+    ALTERADO LARGURA DA BANDA: ${document.querySelector("#largura-2g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#largura-2g-select-1").value} PARA ${document.querySelector("#largura-2g-select-2").value}`} 
+    ALTERADO SGI: ${document.querySelector("#sgi-2g-ligado").checked ? "LIGADO" : "DESLIGADO"} 
+    ALTERADO ENCRIPTAÇÃO: ${document.querySelector("#enctriptacao-2g-alterada-sim").checked ? "SIM" : "NÃO"} 
+
+    REDE 5G 
+    ALTERADO CANAL: ${document.querySelector("#canal-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#canal-5g-select-1").value} PARA ${document.querySelector("#canal-5g-select-2").value}`}  
+    ALTERADO MODO: ${document.querySelector("#modo-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#modo-5g-select-1").value} PARA ${document.querySelector("#modo-5g-select-2").value}`} 
+    ALTERADO LARGURA DA BANDA: ${document.querySelector("#largura-5g-alterado").checked ? "NÃO" : `DE ${document.querySelector("#largura-5g-select-1").value} PARA ${document.querySelector("#largura-5g-select-2").value}`} 
+    ALTERADO SGI: ${document.querySelector("#sgi-5g-ligado").checked ? "LIGADO" : "DESLIGADO"} 
+    ALTERADO ENCRIPTAÇÃO: ${document.querySelector("#enctriptacao-5g-alterada-sim").checked ? "SIM" : "NÃO"} 
+    
+    `    
     modal.show()
 
     if (modal) {
       backdrop.setAttribute("class", "backdrop-activated");
     }
 
-    modalContent.innerHTML = `${serviceContent}`
+    modalContent.innerHTML = `${serviceContent.replace(/\n/g, "<br>")}`
 
     const buttonCopy = document.createElement("button")
     buttonCopy.setAttribute("class", "btn" + " btn-primary");
@@ -87,6 +77,7 @@ export function gerarAtendimento() {
       e.preventDefault();
       modal.close()
       backdrop.setAttribute("class", "backdrop-inactivated");     
+      navigator.clipboard.writeText(serviceContent).then(() => console.log("Copiado!"));
     });
   });
 }
